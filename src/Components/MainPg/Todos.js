f3e5ecdeb2bc0811f2039d40343182952a2f3ead
chetvishal <vishal1982c@gmail.com>
2020-07-29 19:59:49 +0530

    secondary commit

---
+++ src/Components/MainPg/Todos.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Todos = () => {
+    return (
+        <div>
+            <div className="alert alert-primary" role="alert" >
+                A simple primary alert—check it out!
+                <span className="text-right"><a href="#" class="alert-link">Remove</a> </span>
+            </div>
+        </div>
+    )
+}
+
+export default Todos;
