793663131a36be2677ad32d6c100ed4d11299176
chetvishal <vishal1982c@gmail.com>
2020-08-06 21:19:03 +0530

    now firebase data will be extracted from Todolis.js

--- src/App.js
+++ src/App.js
@@ -2,34 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import MainPg from './Components/MainPg/MainPg'
 import AddTodo from './Components/MainPg/AddTodo';
-import {connect} from 'react-redux';
 import TodoList from './Components/MainPg/TodoList';
-import {firestoreConnect} from 'react-redux-firebase';
-import {compose} from 'redux';
 
 class App extends Component {
 
   render() {
-
     return (
       <div className="App">
         <MainPg />
-        <TodoList todos={this.props.todos}/>
+        <TodoList />
         <AddTodo />
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return{
-    todos: state.firestore.ordered.todos
-  }
-}
-
-export default compose(
-  connect(mapStateToProps),
-  firestoreConnect([
-    {collection: 'todos',orderBy: ['createdAt','asc']}
-  ])
-  )(App);
+export default App;
--- src/Components/MainPg/TodoList.js
+++ src/Components/MainPg/TodoList.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Todos from './Todos';
+import {firestoreConnect} from 'react-redux-firebase';
+import {compose} from 'redux';
+import {connect} from 'react-redux';
 
-const TodoList = ({ todos }) => {
-    return (
-        <div className='todolist'>
+class TodoList extends Component {
+    render() {
+        // console.log(this.props.todos)
+        return (
+            <div className='todolist'>
+                {
+                    this.props.todos && this.props.todos.map(todo => {
+                        return (
+                            <div key={todo.id}>
+                                <Todos name={todo.todo} id={todo.id} />
+                            </div>
+                        )
+                    })
+                }
 
-            { 
-                 todos && todos.map(todo => {
-                    return (
-                        <div key={todo.id}>
-                            <Todos name={todo.todo} id={todo.id} />
-                        </div>
-                    )
-                })
-            }
-
-        </div>
-    )
+            </div>
+        )
+    }
 }
-export default TodoList;
+
+const mapStateToProps = (state) => {
+    return{
+        todos: state.firestore.ordered.todos
+      }
+}
+
+ export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+      {collection: 'todos',orderBy: ['createdAt','asc']}
+    ])
+    )(TodoList);
